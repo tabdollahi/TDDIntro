866e57eb7dbd2db0361b2b2e3c007d7444113a67
tabdollahi <abdollahi.tania@gmail.com>
2016-01-02 00:45:47 -0800

    make 4th test pass

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return 1;
+        if (i < 2){
+            return 1;
+        }
+        return i*compute(i - 1);
     }
 }
