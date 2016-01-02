3ae6c5e0aa5053c5ccd1c771cc4bbdc366fa3376
tabdollahi <abdollahi.tania@gmail.com>
2016-01-02 00:52:21 -0800

    make 5th test pass, all other tests still passing

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,9 +2,12 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i < 2){
+        if (i < 0){
+            throw new IllegalArgumentException("i must be a positive number");
+        }
+        if (i < 2) {
             return 1;
         }
-        return i*compute(i - 1);
+        return i * compute(i - 1);
     }
 }
