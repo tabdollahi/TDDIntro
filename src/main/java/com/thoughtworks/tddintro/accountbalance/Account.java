46777a8ed9af48cb2512552398ec24fb8b2fa983
tabdollahi <abdollahi.tania@gmail.com>
2016-01-03 17:20:16 -0800

    make first test pass

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+public class Account {
+    private int balance;
+
+    Account(int balance){
+        this.balance = balance;
+    }
+
+    public int deposit(int amount){
+        balance += amount;
+        return balance;
+    }
+}
--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.tddintro.accountbalance;
 
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        assertThat(new Account(100).deposit(50), is(150));
+    }
 //
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
